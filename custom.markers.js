09192ec76d566b907217eec5d4a2bdb8ab16ff48
Andrew Naber <anaber@Redwood-Mini-3.attlocal.net>
2025-07-17 23:08:06 -0700

    Initial Minecraft map upload

---
+++ custom.markers.js
@@ -0,0 +1,20 @@
+var UnminedCustomMarkers = {
+    isEnabled: true,
+    markers: [
+        { x: 73, z: 31, title: "Base One", description: "Y: 67" },
+        { x: -297, z: 347, title: "Acacia Village 1", description: "Y: 62" },
+        { x: 663, z: -2177, title: "Unknown Base", description: "Y: 70" },
+        { x: 806, z: -1042, title: "Strong Hold Village", description: "Y: 62" },
+        { x: 2840, z: -4296, title: "Taiga Mineshaft Base", description: "Y: 65" },
+        { x: 3019, z: -4060, title: "Taiga Big Cave", description: "Y: 68" },
+        { x: 1492, z: -1996, title: "Ice Mountain", description: "Y: 106" },
+        { x: -51, z: -2918, title: "Start of Tunnel to Ancient City", description: "Y: -49" },
+        { x: 600, z: -2918, title: "Ancient City One", description: "Y: -51" },
+        { x: -51, z: -2000, title: "Long Tunnel Start", description: "Y: -49" },
+        { x: 1177, z: -3020, title: "Ancient City Two Water Elevator", description: "Y: 111" },
+        { x: 1191, z: -2962, title: "Ancient City Two Nether Portal", description: "Y: -51" },
+        { x: -43, z: -1950, title: "Bad Lands Edge Base", description: "Y: 96" },
+        { x: -716, z: -1984, title: "Dead Desert Village", description: "Y: 73" },
+        { x: 1366, z: -2073, title: "Unknown Village", description: "Y: 94" }
+    ]
+};
